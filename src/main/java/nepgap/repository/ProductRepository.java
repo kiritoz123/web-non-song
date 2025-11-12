77a6fb5c46dd44dc58359d671dce4b7f9b836ef7
HuyTQ <huytq@hblab.vn>
2025-11-12 20:56:18 +0700

    fix

--- src/main/java/nepgap/repository/ProductRepository.java
+++ src/main/java/nepgap/repository/ProductRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    List<Product> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String desc);
 
     @Query("select p.id, p.name, p.description, p.price,p.locale,p.stock,p.status,pi.url from Product p LEFT JOIN ProductImage pi where (:q is null or lower(p.name) like lower(concat('%',:q,'%')) or lower(p.description) like lower(concat('%',:q,'%')))")
     List<ProductProjection> searchByKeyword(String q);
